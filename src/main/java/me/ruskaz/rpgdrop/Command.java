652df10dd87f903f146f68bd5e696324d0a6e694
RuSKazVG <rkazakov2006@gmail.com>
2024-05-25 22:43:39 +0400

    forgor those

--- src/main/java/me/ruskaz/rpgdrop/Command.java
+++ src/main/java/me/ruskaz/rpgdrop/Command.java
@@ -1,21 +1,13 @@
 package me.ruskaz.rpgdrop;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.Objects;
-
-import static io.papermc.paper.text.PaperComponents.plainSerializer;
 
 public class Command implements CommandExecutor {
 
@@ -28,28 +20,18 @@ public class Command implements CommandExecutor {
             return true;
         }
         if (args[0].equalsIgnoreCase("clear")) {
-            for (World world : Bukkit.getServer().getWorlds()) {
-                for (Item entity : world.getEntitiesByClass(Item.class)) {
-                    ItemStack item = entity.getItemStack();
-                    if (item.hasItemMeta() && item.getItemMeta().hasLore()) {
-                        ItemMeta meta = item.getItemMeta();
-                        List<Component> lore = meta.lore();
-                        for (int i = 0; i < lore.size(); i++) {
-                            String l = plainSerializer().serialize(lore.get(i));
-                            if (!l.contains("affected")) continue;
-                            lore = ItemOperations.clearLore(lore);
-                            meta.lore(lore);
-                            item.setItemMeta(meta);
-                            break;
-                        }
-                    }
-                }
-            }
+            RPGDrop.droppedItems.clear();
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("clearProtectionMessage"))));
         }
         else if (args[0].equalsIgnoreCase("reload")) {
             plugin.reloadConfig();
+            RPGDrop.config = plugin.getConfig();
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("configReloadMessage"))));
+        }
+        else if (args[0].equalsIgnoreCase("kys")) {
+            for (ItemStack item : RPGDrop.droppedItems.keySet()) {
+                sender.sendMessage(item.getType().name());
+            }
         }
         else {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(plugin.getConfig().getString("illegalArgumentsMessage"))));
